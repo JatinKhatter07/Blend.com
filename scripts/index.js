1f7942f5d7e08ab83538d8ac693d003a1f4d743a
Godoffcode <jatinkhatter0789@gmail.com>
2022-11-03 13:33:25 +0530

    added : Project-fitnessBlender

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+import { myheader, myfooter  } from "../components/components.js";
+
+let latestUpdate = document.querySelector('#new_update > div #close_symbol');
+latestUpdate.addEventListener('click', () => {
+    document.getElementById('new_update').innerHTML= null;
+});
+// console.log(latestUpdate);
+
+let header = document.querySelector('#navbar');
+header.innerHTML = myheader();
+
+let footer = document.querySelector('main+footer');
+footer.innerHTML = myfooter();
